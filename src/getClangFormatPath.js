62f4ca87df6006c3eb7e262f1965b7af7109f5af
루밀LuMir <rpfos@naver.com>
2024-09-05 23:36:39 +0900

    feat: create getClangFormatPath.js

---
+++ src/getClangFormatPath.js
@@ -0,0 +1,27 @@
+const { existsSync } = require('fs');
+const { resolve } = require('path');
+
+/**
+ * Resolves and returns the path to the `clang-format` executable based on the OS platform and architecture.
+ * Throws an error if the executable is not found.
+ *
+ * @param {string} osPlatform The current operating system platform (e.g., 'darwin', 'linux', 'win32').
+ * @param {string} architecture The system architecture (e.g., 'arm64', 'x64').
+ * @returns {string} The resolved path to the `clang-format` executable.
+ * @throws {Error} Throws an error if the executable is not found for the specified OS platform and architecture.
+ */
+module.exports = (osPlatform, architecture) => {
+  const clangFormatPath = resolve(
+    __dirname,
+    `bin`,
+    `${osPlatform}-${architecture}`,
+    `clang-format${osPlatform === 'win32' ? '.exe' : ''}`,
+  );
+
+  if (!existsSync(clangFormatPath))
+    throw new Error(
+      `No executable found for '${osPlatform}(OS platform)-${architecture}(architecture)'\nSee supported OS platform and architecture lists of clang-format-node in README.md`,
+    );
+
+  return clangFormatPath;
+};
