ba2f7713deb8391290020b98b72fc009fd88f6b5
루밀LuMir <rpfos@naver.com>
2024-11-07 18:09:37 +0900

    wip: update `clangFormatPath.test.js`

--- packages/clang-format-node/src/utils/clangFormatPath.test.js
+++ packages/clang-format-node/src/utils/clangFormatPath.test.js
@@ -2,11 +2,14 @@ const { strictEqual } = require('node:assert');
 const { platform, arch } = require('node:os');
 const { describe, it } = require('node:test');
 
-const { clangFormatPath } = require('./clangFormatPath');
+const { clangFormatPath, clangFormatNodePath } = require('./clangFormatPath');
 const { getClangFormatPath } = require('./getClangFormatPath');
 
 describe('clangFormatPath strictEqual testing', () => {
   it('clangFormatPath === getClangFormatPath(platform(), arch())', () => {
     strictEqual(clangFormatPath, getClangFormatPath(platform(), arch()));
+  });
+  it('clangFormatNodePath === clangFormatPath', () => {
+    strictEqual(clangFormatNodePath, clangFormatPath);
   });
 });
