7ad27e5051d66fff10874f0ed1c4f9b42ae1813c
루밀LuMir <rpfos@naver.com>
2024-11-07 18:44:30 +0900

    feat(clang-forma-node): add new API aliases `clangFormatNodePath` and `getClangFormatNodePath` (#83)
    * chore: update `package.json`'s `bin`
    
    * wip: update `clangFormatPath.js`
    
    * wip: update `clangFormatPath.test.js`
    
    * wip: update `getClangFormatPath.js`
    
    * wip: update `getClangFormatPath.test.js`
    
    * wip: update `index.js`
    
    * wip: update `index.test.js`

--- packages/clang-format-node/src/index.test.js
+++ packages/clang-format-node/src/index.test.js
@@ -1,13 +1,24 @@
 const { ok } = require('node:assert');
 const { describe, it } = require('node:test');
 
-const { clangFormatPath, getClangFormatPath } = require('./index');
+const {
+  clangFormatPath,
+  clangFormatNodePath,
+  getClangFormatPath,
+  getClangFormatNodePath,
+} = require('./index');
 
 describe('index ok testing', () => {
   it('clangFormatPath should be imported correctly', () => {
     ok(clangFormatPath);
   });
+  it('clangFormatNodePath should be imported correctly', () => {
+    ok(clangFormatNodePath);
+  });
   it('getClangFormatPath should be imported correctly', () => {
     ok(getClangFormatPath);
+  });
+  it('getClangFormatNodePath should be imported correctly', () => {
+    ok(getClangFormatNodePath);
   });
 });
--- packages/clang-format-node/src/utils/clangFormatPath.js
+++ packages/clang-format-node/src/utils/clangFormatPath.js
@@ -4,9 +4,21 @@ const { getClangFormatPath } = require('./getClangFormatPath');
 
 /**
  * The ABSOLUTE path to the `clang-format` executable binary based on the OS platform and architecture.
+ *
+ * @alias `clangFormatNodePath`. See {@link clangFormatNodePath}.
+ * @version `v1.2.0` Initial release.
  */
 const clangFormatPath = getClangFormatPath(platform(), arch());
 
+/**
+ * Alias for `clangFormatPath`.
+ *
+ * @alias `clangFormatPath`. See {@link clangFormatPath}.
+ * @version `v1.2.0` Initial release.
+ */
+const clangFormatNodePath = clangFormatPath;
+
 module.exports = {
   clangFormatPath,
+  clangFormatNodePath,
 };
