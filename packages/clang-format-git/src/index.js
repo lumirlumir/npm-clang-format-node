12cf3e80abe077d444dce7a70b42d2d430f27266
루밀LuMir <rpfos@naver.com>
2024-11-07 19:34:45 +0900

    wip: create `index.js`

---
+++ packages/clang-format-git/src/index.js
@@ -0,0 +1,12 @@
+const {
+  getGitClangFormatPath,
+  getClangFormatGitPath,
+} = require('./utils/getGitClangFormatPath');
+const { gitClangFormatPath, clangFormatGitPath } = require('./utils/gitClangFormatPath');
+
+module.exports = {
+  getGitClangFormatPath,
+  getClangFormatGitPath,
+  gitClangFormatPath,
+  clangFormatGitPath,
+};
