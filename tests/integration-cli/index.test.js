c0218bfb4b55732e410044332558bee0bc67b9c0
루밀LuMir <rpfos@naver.com>
2024-11-29 22:45:10 +0900

    wip

--- tests/integration-cli/index.test.js
+++ tests/integration-cli/index.test.js
@@ -1,10 +1,9 @@
-const { doesNotThrow, throws } = require('node:assert');
+const { doesNotThrow } = require('node:assert');
 const { execSync } = require('node:child_process');
 const { describe, it } = require('node:test');
 
-describe('integration-cli doesNotThrow and throws testing', () => {
+describe('integration-cli doesNotThrow testing', () => {
   describe('clang-format-node', () => {
-    // doesNotThrow
     it('--help', () => {
       doesNotThrow(() => {
         execSync(`npx clang-format-node --help`);
@@ -15,43 +14,20 @@ describe('integration-cli doesNotThrow and throws testing', () => {
         execSync(`npx clang-format-node --version`);
       });
     });
-
-    // Throws
-    it('--abcdefg', () => {
-      throws(() => {
-        execSync(`npx clang-format-node --abcdefg`);
-      });
-    });
   });
 
   describe('clang-format-git', () => {
-    // doesNotThrow
     it('--help', () => {
       doesNotThrow(() => {
         execSync(`npx clang-format-git --help`);
       });
     });
-
-    // Throws
-    it('--abcdefg', () => {
-      throws(() => {
-        execSync(`npx clang-format-git --abcdefg`);
-      });
-    });
   });
 
   describe('clang-format-git-python', () => {
-    // doesNotThrow
     it('--help', () => {
       doesNotThrow(() => {
         execSync(`npx clang-format-git-python --help`);
-      });
-    });
-
-    // Throws
-    it('--abcdefg', () => {
-      throws(() => {
-        execSync(`npx clang-format-git-python --abcdefg`);
       });
     });
   });
