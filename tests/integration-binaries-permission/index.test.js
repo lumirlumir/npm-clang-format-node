6ac228e6ea788a5c6e4d132b38dfd0fd7f307107
루밀LuMir <rpfos@naver.com>
2024-11-30 19:12:53 +0900

    test: create `integration-binaries-permission` test (#121)
    * wip
    
    * wip
    
    * Delete .github/workflows/temp.yml

---
+++ tests/integration-binaries-permission/index.test.js
@@ -0,0 +1,75 @@
+const { ok } = require('node:assert');
+const { execSync } = require('node:child_process');
+const { describe, it } = require('node:test');
+const { platform } = require('node:os');
+
+const { getClangFormatNodePath } = require('clang-format-node');
+const { getClangFormatGitPath } = require('clang-format-git');
+const { clangFormatGitPythonPath } = require('clang-format-git-python');
+
+const isPermission755 = path =>
+  execSync(`stat -c '%a' ${path}`).toString().trim() === '755';
+
+const binaries = [
+  {
+    osPlatform: 'darwin',
+    architecture: 'arm64',
+  },
+  {
+    osPlatform: 'darwin',
+    architecture: 'x64',
+  },
+  {
+    osPlatform: 'linux',
+    architecture: 'arm',
+  },
+  {
+    osPlatform: 'linux',
+    architecture: 'arm64',
+  },
+  {
+    osPlatform: 'linux',
+    architecture: 'ppc64',
+  },
+  {
+    osPlatform: 'linux',
+    architecture: 's390x',
+  },
+  {
+    osPlatform: 'linux',
+    architecture: 'x64',
+  },
+  {
+    osPlatform: 'win32',
+    architecture: 'x64',
+  },
+];
+
+describe('integration-binaries-permission ok testing', () => {
+  // skip test on Windows.
+  if (platform() === 'win32') {
+    // eslint-disable-next-line no-console
+    console.log('This test was skipped on Windows.');
+    return;
+  }
+
+  describe('clang-format-node', () => {
+    binaries.forEach(({ osPlatform, architecture }) => {
+      it(`osPlatform: ${osPlatform}, architecture: ${architecture}`, () => {
+        ok(isPermission755(getClangFormatNodePath(osPlatform, architecture)));
+      });
+    });
+  });
+
+  describe('clang-format-git', () => {
+    binaries.forEach(({ osPlatform, architecture }) => {
+      it(`osPlatform: ${osPlatform}, architecture: ${architecture}`, () => {
+        ok(isPermission755(getClangFormatGitPath(osPlatform, architecture)));
+      });
+    });
+  });
+
+  it('clang-format-git-python', () => {
+    ok(isPermission755(clangFormatGitPythonPath));
+  });
+});
