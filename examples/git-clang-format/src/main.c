4f6123e744ff5f2f73b5b91abd1e219b6f4144f7
루밀LuMir <rpfos@naver.com>
2025-01-03 22:04:39 +0900

    wip

---
+++ examples/git-clang-format/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+              printf("Line 4");
+              printf("Line 5");
+              pritnf("Line 6");
+              pritnf("Line 7");
+              pritnf("Line 8");
+              pritnf("Line 9");
+              pritnf("Line 10");
+
+              return 0;
+}
