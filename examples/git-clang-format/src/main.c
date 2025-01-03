a5f7c204777323232f11faa0f4fdd34f8ea15f88
루밀LuMir <rpfos@naver.com>
2025-01-03 22:07:57 +0900

    wip

--- examples/git-clang-format/src/main.c
+++ examples/git-clang-format/src/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-              printf("Line 4");
-              printf("Line 5");
-              pritnf("Line 6");
-              pritnf("Line 7");
-              pritnf("Line 8");
-              pritnf("Line 9");
-              pritnf("Line 10");
+  printf("Line 4");
+  printf("Line 5");
+  pritnf("Line 6");
+      pritnf("Line 7");
+  pritnf("Line 8");
+            pritnf("Line 9");
+  pritnf("Line 10");
 
-              return 0;
+  return 0;
 }
