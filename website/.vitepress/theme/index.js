35dcdd64908a1640f293434991e134889c1c850a
루밀LuMir <rpfos@naver.com>
2025-01-23 21:21:56 +0900

    wip: add documentation comments to VitePress theme entry file

--- website/.vitepress/theme/index.js
+++ website/.vitepress/theme/index.js
@@ -1,7 +1,21 @@
+/**
+ * @fileoverview VitePress theme entry.
+ *
+ * @see https://vitepress.dev/guide/custom-theme#using-a-custom-theme
+ */
+
+// --------------------------------------------------------------------------------
+// Import
+// --------------------------------------------------------------------------------
+
 import theme from 'vitepress/theme';
 
 import './style.css';
 import 'virtual:group-icons.css'; // eslint-disable-line n/no-missing-import
+
+// --------------------------------------------------------------------------------
+// Export
+// --------------------------------------------------------------------------------
 
 /** @type {import('vitepress').Theme} */
 export default theme;
