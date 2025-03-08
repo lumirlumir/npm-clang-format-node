6314c1a951c8554095c0225f94d60667ff91e0a2
lumirlumir <rpfos@naver.com>
2025-03-08 10:54:52 +0000

    chore(sync-server): synced local './lint-staged.config.js' with remote './configs/lint-staged.config.js'

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,6 +3,6 @@ module.exports = {
     'npx prettier --check',
     'npx editorconfig-checker -config .editorconfig-checker.json',
   ],
-  '*.{js,mjs,cjs,jsx}': 'npx eslint',
+  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': 'npx eslint',
   '*.md': ['npx markdownlint', 'npx textlint -f pretty-error'],
 };
