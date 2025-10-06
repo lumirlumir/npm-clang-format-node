66b597935129174723afc8c943c073bd0adde853
lumirlumir <rpfos@naver.com>
2025-10-06 05:27:21 +0000

    chore(sync-server): synced local './eslint.config.mjs' with remote './configs/eslint.config.mjs'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,15 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
 import bananass from 'eslint-config-bananass';
 import mark from 'eslint-plugin-mark';
 
 /** @type {import("eslint").Linter.Config[]} */
-export default [
-  {
-    name: 'global/ignores',
-    ignores: ['**/build/', '**/coverage/', '**/.vitepress/cache/'],
-  },
+export default defineConfig([
+  globalIgnores(['**/build/', '**/coverage/', '**/.vitepress/cache/'], 'global/ignores'),
+
   bananass.configs.js,
   bananass.configs.ts,
+  bananass.configs.json,
+  bananass.configs.jsonc,
+  bananass.configs.json5,
   mark.configs.recommendedGfm,
-];
+]);
