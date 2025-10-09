38b6c4a473388d981baa6c74fdd085d57ddc3be2
루밀LuMir <rpfos@naver.com>
2025-10-09 21:11:32 +0900

    chore(sync-server): add `json`, `jsonc`, and `json5` to `lint-staged` config (#480)
    synced local file(s) with
    [lumirlumir/lumirlumir-configs](https://github.com/lumirlumir/lumirlumir-configs).
    
    @lumirlumir check out
    [`182e772`](https://github.com/lumirlumir/lumirlumir-configs/commit/182e772f649a24affc7da5bfcaa817e4acd414f6).🐣
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>synced local <code>./lint-staged.config.mjs</code> with remote
    <code>./configs/lint-staged.config.mjs</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#18375679774](https://github.com/lumirlumir/lumirlumir-configs/actions/runs/18375679774)

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,6 +3,6 @@ export default {
     'npx prettier --write --ignore-unknown',
     'npx editorconfig-checker -config .editorconfig-checker.json',
   ],
-  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,md}': 'npx eslint --fix',
+  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,json,jsonc,json5,md}': 'npx eslint --fix',
   '*.md': ['npx markdownlint --fix', 'npx textlint -f pretty-error'],
 };
