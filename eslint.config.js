d3e4a83c932b154d9dea610402250638474cf21b
lumirlumir <yonghyeon0324@gmail.com>
2026-06-01 06:54:08 +0000

    chore(sync-server): synced local './eslint.config.js' with remote './configs/eslint.config.js'

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,22 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
-import bananass from 'eslint-config-bananass';
+import js from 'eslint-config-bananass/js';
+import ts from 'eslint-config-bananass/ts';
+import json from 'eslint-config-bananass/json';
+import jsonc from 'eslint-config-bananass/jsonc';
+import json5 from 'eslint-config-bananass/json5';
 import md from 'eslint-markdown';
 
-/** @type {import("eslint").Linter.Config[]} */
 export default defineConfig([
   globalIgnores(
     ['**/build/', '**/coverage/', '**/.vitepress/.temp/', '**/.vitepress/cache/'],
     'global/ignores',
   ),
 
-  bananass.configs.js,
-  bananass.configs.ts,
-  bananass.configs.json,
-  bananass.configs.jsonc,
-  bananass.configs.json5,
+  js,
+  ts,
+  json,
+  jsonc,
+  json5,
   md.configs.recommended,
   md.configs.stylistic,
 
